cc9cdb51b40091dd7321a8f657ab43959d0d104a
gabriel Salermo <enzogabrielskull@gmail.com>
2023-09-04 19:00:51 -0300

    Exercicio 8

---
+++ Documents/PROGRAMAÇÃO - ESTUDOS/Exerciciospy/desafio 8 - aula 7.py
@@ -0,0 +1,4 @@
+metro = int(input('Me dê um valor inteiro em metros: '))
+cm = metro * 100
+ml = metro * 1000
+print(f'o valor em centimentros é equivalente a: {cm} centimetros \no valor em milimetros é equivalente a: {ml} milimetros')
